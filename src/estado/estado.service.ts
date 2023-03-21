d3de5e182881575296bed92d9b8ade7806383753
israhb <heber_ji@hotmail.com>
2023-03-20 18:55:36 -0600

    ordenamiento para estados

--- src/estado/estado.service.ts
+++ src/estado/estado.service.ts
@@ -35,7 +35,10 @@ export class EstadoService {
     return this.estadoRepository.find({
       where: {
         activo: 1
-      }
+      },
+      order:{
+        id: "DESC"
+      },
     });
   }
 
