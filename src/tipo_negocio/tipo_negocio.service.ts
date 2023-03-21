274779c901f96147b3874d351b9b4f08ec15ed7d
israhb <heber_ji@hotmail.com>
2023-03-21 08:46:30 -0600

    agregar ordenamientode tipos de negocio

--- src/tipo_negocio/tipo_negocio.service.ts
+++ src/tipo_negocio/tipo_negocio.service.ts
@@ -35,7 +35,10 @@ export class TipoNegocioService {
     return this.tipoNegocioRepository.find({
       where:{
         activo: 1
-      }
+      },
+      order:{
+        id: "DESC"
+      },
     });
   }
 
